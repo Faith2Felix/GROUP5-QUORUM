8bf18948da5d1968f437eb484454e945d8e8d234
Adedayo Oluwatobi <omoade747@gmail.com>
2026-05-30 01:30:14 +0100

    js edit

--- JS/SignUp.js
+++ JS/SignUp.js
@@ -55,16 +55,16 @@
         } )
 
         function signUp() {
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirmPassword").value;
+        const password = document.getElementById("password").value;
+        const confirmPassword = document.getElementById("confirmPassword").value;
 
-    if (password !== confirmPassword) {
-        alert("Passwords do not match. Please try again.");
+        if (password !== confirmPassword) {
+            alert("Passwords do not match. Please try again.");
         return;
-    }
+        }
 
 
-    }
+        }
 
 
 
